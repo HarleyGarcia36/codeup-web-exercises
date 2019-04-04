6e6b8efae8427f329e0e24db3244f90ef134802f
Harley Garcia <orion@Harleys-MacBook-Air.local>
2019-04-04 14:30:51 -0500

    ok

--- js/conditionals.js
+++ js/conditionals.js
@@ -16,6 +16,26 @@
  * Can you refactor your code to use functions?
  */
 
+// var enterANum = confirm("Would you like to enter a number.");
+//     if (enterANum === true) {
+//     var num = prompt("Enter a number.");
+//
+//         if (num % 2 === 0) {
+//             alert(num + " is an even number")
+//         } else {
+//             alert(num + " is an odd number.")
+//         }
+//
+//         alert("The number plus one-hundred is " + (+num + 100) + ".");
+//
+//         if (num < 0) {
+//             alert("The number is negative.")
+//         } else {
+//             alert("The number is positive.")
+//         }
+//     }
+
+
 /* ########################################################################## */
 
 /**
@@ -35,6 +55,9 @@
  * Test your function by passing various string literals to it and
  * console.logging the function's return value
  */
+// function analyzeColor(color) {
+//     return color
+// }
 
 // Don't change the next two lines!
 // These lines create two variables for you:
@@ -48,7 +71,11 @@ var randomColor = colors[Math.floor(Math.random() * colors.length)];
  * Pass the `randomColor` variable to your function and console.log the results.
  * You should see a different message everytime you refresh the page
  */
+function rColor(color) {
+    return randomColor
+}
 
+console.log(rColor())
 /**
  * TODO:
  * Refactor your above function to use a switch-case statement
