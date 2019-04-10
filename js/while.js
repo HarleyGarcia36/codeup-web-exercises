041bfd542750b8d8e49e3feb49ea720aaf63229d
Harley Garcia <orion@Harleys-MacBook-Air.local>
2019-04-10 14:38:04 -0500

    ok

---
+++ js/while.js
@@ -0,0 +1,21 @@
+// var i = 1;
+//
+// function squareOfNumber(a) {
+//         return a * a;
+//     }
+//
+// while (i % 2 === 0){
+//     i++;
+// }
+//
+// console.log(squareOfNumber(i))
+//
+// var i = 1;
+// var allCones = Math.floor(Math.random() * 50) + 50;
+//
+//
+//
+// do {
+//     console.log()
+// }
+// while ()
