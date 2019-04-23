ff9d8a03efa998949097df4d93d9785a4bdc497d
Harley Garcia <orion@Harleys-MacBook-Air.local>
2019-04-23 10:09:24 -0500

    jquery selectors

---
+++ js/custom_jquery.js
@@ -0,0 +1,15 @@
+// var contents = $('#welcome-header').html();
+// alert(contents);
+
+// $('.codeup').css('border', '1px solid red');
+
+// $('li').css('font-size', '20px');
+
+// $('h1').css('background', 'blue');
+// $('p').css('background', 'blue');
+// $('li').css('background', 'blue');
+
+// var contents = $('h1').html();
+// alert(contents);
+
+$('h1, p, li').css('background', 'blue');
