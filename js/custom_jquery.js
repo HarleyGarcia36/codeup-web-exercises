667dc83b6f86dac5b9457a5a40dc39bcfbec7bdc
Harley Garcia <orion@Harleys-MacBook-Air.local>
2019-04-23 11:24:05 -0500

    jquery practice

--- js/custom_jquery.js
+++ js/custom_jquery.js
@@ -1,15 +1,40 @@
+//exercise1:
+// $(alert('The DOM has finished loading'));
+
+
+//exercise2:
 // var contents = $('#welcome-header').html();
 // alert(contents);
 
-// $('.codeup').css('border', '1px solid red');
+$('.codeup').css('border', '1px solid red');
 
-// $('li').css('font-size', '20px');
+$('li').css('font-size', '20px');
 
 // $('h1').css('background', 'blue');
 // $('p').css('background', 'blue');
 // $('li').css('background', 'blue');
 
-// var contents = $('h1').html();
-// alert(contents);
+var contents = $('h1').html();
+alert(contents);
 
-$('h1, p, li').css('background', 'blue');
+$('h1, p, li').css('background', '#007bfc');
+
+
+//practice:
+// $('*').html('ok den');
+
+// $('#welcome-header').css('color', '#00b400');
+
+// $('li:nth-child(2)').css('color', '#007bfc');
+
+// $('h1').css('color', '#ff0001');
+
+// var enterColor = $(prompt('enter a color'));
+
+// $('#the-rules').css('border', '5px solid gold');
+
+// $('div').width(10);
+
+// jQuery('#welcome-header').html('<h2>Huhhhh</h2>');
+
+// jQuery('h2').css('color', '#00ff00');
